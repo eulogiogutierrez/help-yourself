00bcecdd87f1cb023b50df7cc4db994dc3043cb9
Eulogio Gutierrez <eulogio.gutierrez@digital.hmrc.gov.uk>
2018-02-26 12:22:12 +0000

    more clean up

--- app/controllers/WhatDoYouWantToDoController.scala
+++ app/controllers/WhatDoYouWantToDoController.scala
@@ -10,7 +10,7 @@ class WhatDoYouWantToDoController @Inject() extends Controller {
 
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
 }
